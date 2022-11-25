867e7e52bbe7113979796fa8a1de88f0cd7cd194
“HoangHaiSon” <sonhoanghai00@gmail.com>
2022-11-25 21:57:56 +0700

    xong

--- Pay1193/Controllers/PaymentRecordController.cs
+++ Pay1193/Controllers/PaymentRecordController.cs
@@ -14,15 +14,6 @@ namespace Pay1193.Controllers
         private readonly INationalInsuranceService _nationalInsuranceService;
         private readonly IPayService _payService;
 
-        //private decimal overtimeHrs;
-        //private decimal contractualEarnings;
-        //private decimal overtimeEarnings;
-        //private decimal totalEarnings;
-        //private decimal tax;
-        //private decimal unionFee;
-        //private decimal studentLoan;
-        //private decimal nationalInsurance;
-        //private decimal totalDeduction;
 
         public PaymentRecordController(IPaymentRecord paymentRecordService, IEmployee employeeService, ITaxService taxService, INationalInsuranceService nationalInsuranceService, IPayService payService)
         {
@@ -63,8 +54,6 @@ namespace Pay1193.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(PaymentRecordCreateViewModel model)
         {
-            //if (ModelState.IsValid)
-            //{
             var paymentRecord = new PaymentRecord
             { 
                 EmployeeId = model.EmployeeId,
@@ -84,134 +73,6 @@ namespace Pay1193.Controllers
 
             await _paymentRecordService.CreateAsync(paymentRecord);
             return RedirectToAction("Index");
-            //}
-            //return View();
         }
-
-        //public IActionResult Detail(int id)
-        //{
-        //    var PaymentRecord = _PaymentRecordService.GetById(id);
-        //    if (PaymentRecord == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    PaymentRecordDetailsViewModel model = new PaymentRecordDetailsViewModel()
-        //    {
-        //        Id = PaymentRecord.Id,
-        //        PaymentRecordNo = PaymentRecord.PaymentRecordNo,
-        //        FullName = PaymentRecord.FullName,
-        //        Gender = PaymentRecord.Gender,
-        //        ImageUrl = PaymentRecord.ImageUrl,
-        //        DoB = PaymentRecord.DoB,
-        //        Designation = PaymentRecord.Designation,
-        //        Email = PaymentRecord.Email,
-        //        NationalInsuranceNo = PaymentRecord.NationalInsuranceNo,
-        //        PaymentMethod = PaymentRecord.PaymentMethod,
-        //        StudentLoan = PaymentRecord.StudentLoan,
-        //        UnionMember = PaymentRecord.UnionMember,
-        //        Address = PaymentRecord.Address,
-        //        City = PaymentRecord.City,
-        //        PostCode = PaymentRecord.PostCode
-        //    };
-        //    return View(model);
-        //}
-
-        //[HttpGet]
-        //public IActionResult Edit(int id)
-        //{
-        //    var PaymentRecord = _PaymentRecordService.GetById(id);
-        //    if (PaymentRecord == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    var model = new PaymentRecordEditViewModel()
-        //    {
-        //        Id = PaymentRecord.Id,
-        //        FirstName = PaymentRecord.FirstName,
-        //        LastName = PaymentRecord.LatName,
-        //        MidleName = PaymentRecord.MiddleName,
-        //        PaymentRecordNo = PaymentRecord.PaymentRecordNo,
-        //        Gender = PaymentRecord.Gender,
-        //        Email = PaymentRecord.Email,
-        //        DOB = PaymentRecord.DoB,
-        //        DateJoined = PaymentRecord.DateJoined,
-        //        Designation = PaymentRecord.Designation,
-        //        NationalInsuranceNo = PaymentRecord.NationalInsuranceNo,
-        //        PaymentMethod = PaymentRecord.PaymentMethod,
-        //        StudentLoan = PaymentRecord.StudentLoan,
-        //        UnionMember = PaymentRecord.UnionMember,
-        //        Address = PaymentRecord.Address,
-        //        City = PaymentRecord.City,
-        //        Postcode = PaymentRecord.PostCode
-        //    };
-        //    return View(model);
-        //}
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(PaymentRecordEditViewModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var PaymentRecord = new PaymentRecord
-        //        {
-        //            Id = model.Id,
-        //            FirstName = model.FirstName,
-        //            MiddleName = model.MidleName,
-        //            LatName = model.LastName,
-        //            Gender = model.Gender,
-        //            DoB = model.DOB,
-        //            DateJoined = model.DateJoined,
-        //            Designation = model.Designation,
-        //            NationalInsuranceNo = model.NationalInsuranceNo,
-        //            StudentLoan = model.StudentLoan,
-        //            PaymentMethod = model.PaymentMethod,
-        //            UnionMember = model.UnionMember,
-        //            Phone = model.Phone,
-        //            Address = model.Address,
-        //            City = model.City,
-        //            PostCode = model.Postcode,
-        //            Email = model.Email,
-        //            PaymentRecordNo = model.PaymentRecordNo,
-        //            FullName = model.FullName
-        //        };
-        //        if (model.ImageUrl != null & model.ImageUrl.Length > 0)
-        //        {
-        //            var uploadDir = @"images/PaymentRecords";
-        //            var filename = Path.GetFileNameWithoutExtension(model.ImageUrl.FileName);
-        //            var extension = Path.GetExtension(model.ImageUrl.FileName);
-        //            var webrootPath = _webHostEnvironment.WebRootPath;
-        //            filename = DateTime.UtcNow.ToString("yymmssfff") + filename + extension;
-        //            var path = Path.Combine(webrootPath, uploadDir, filename);
-        //            await model.ImageUrl.CopyToAsync(new FileStream(path, FileMode.Create));
-        //            PaymentRecord.ImageUrl = "/" + uploadDir + "/" + filename;
-
-        //        }
-        //        await _PaymentRecordService.UpdateAsync(PaymentRecord);
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View();
-        //}
-        //[HttpGet]
-        //public IActionResult Delete(int id)
-        //{
-        //    var PaymentRecord = _PaymentRecordService.GetById(id);
-        //    if (PaymentRecord == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    var model = new PaymentRecordDeleteViewModel
-        //    {
-        //        Id = PaymentRecord.Id,
-        //        FullName = PaymentRecord.FullName
-        //    };
-        //    return View(model);
-        //}
-        //[HttpPost]
-        //public async Task<IActionResult> Delete(PaymentRecordDeleteViewModel model)
-        //{
-        //    await _PaymentRecordService.Delete(model.Id);
-        //    return RedirectToAction("Index");
-        //}
     }
 }
